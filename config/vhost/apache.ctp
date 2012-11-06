14fc9df3d9b9b246bf2a1dd193dc4073d6331351
Jan Ptacek <jan.ptacek@gmail.com>
2012-11-06 16:27:31 +0100

    server configs

---
+++ config/vhost/apache.ctp
@@ -0,0 +1,25 @@
+NameVirtualHost *:80
+
+<VirtualHost *:80>
+    DocumentRoot "<?php echo $webroot ?>"
+    ServerName    <?php echo $domain  ?> 
+
+    ErrorLog  "<?php echo $logpath ?>apache-error.log"
+    LogLevel   warn
+    CustomLog "<?php echo $logpath ?>apache-access.log" combined
+    #SetEnvIf Request_URI ".(ico|pdf|flv|jpg|jpeg|png|gif|js|css|gz|swf|txt)$" dontlog
+    #CustomLog "<?php echo $logpath ?>apache-access.log" combined env=!dontlog
+
+    <Directory "<?php echo $webroot ?>">
+        Options MultiViews
+        AllowOverride All
+        Order allow,deny
+        allow from all
+        Options +FollowSymLinks +SymLinksIfOwnerMatch
+        
+        RewriteEngine On
+        RewriteCond %{REQUEST_FILENAME} !-d
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteRule ^(.*)$ index.php?url=$1 [QSA,L]
+    </Directory>
+</VirtualHost>
---
+++ vendors/shells/vhost.php
@@ -0,0 +1,62 @@
+<?php
+Class VhostShell extends Shell {
+
+	public function main() {
+		if (empty($this->args)) {
+			$this->help();
+			return;
+		}
+		
+		$this->create();
+	}
+
+	/**
+	 * help
+	 *
+	 */
+	public function help() {
+		$this->hr();
+		$this->out('Usage: cake vhost create <vhostname> [-t <template>]');
+		$this->hr();
+		$this->out('Parameters:');
+		$this->out('	<vhostname>');
+		$this->out('		(eg. vhost.example.com)');
+		$this->out();
+		$this->out('	<template>');
+		$this->out('		template file. (default: "apache")');
+		$this->out();
+		$this->hr();
+	}
+
+	public function create() {
+		// vars used in the template
+		$domain  = 'klik';//$this->args[0];
+		$webroot = WWW_ROOT;
+		$logpath = ROOT . DS . APP_DIR . DS . 'tmp' . DS . 'logs' . DS;
+
+		$template = 'apache';
+		if (!empty($this->params['t']) && is_file($this->_getTemplatePath($this->params['t']) . $this->params['t'] . '.ctp')) {
+			$template = $this->params['t'];
+		}
+
+		ob_start();
+		include $this->_getTemplatePath($template) . $template . '.ctp';
+		$conf = ob_get_contents();
+		ob_end_clean();
+
+		$this->out($conf);
+		
+		$this->createFile(ROOT . DS . APP_DIR . DS . 'config' . DS . 'vhost' . DS . $domain, $conf);
+	}
+
+	protected function _getTemplatePath($template='') {
+		$app_vhost = ROOT . DS . APP_DIR . DS . 'config' . DS . 'vhost' . DS;
+		if (is_file($app_vhost . $template . '.ctp')) {
+			// app config/vhost/{template}.ctp
+			return $app_vhost;
+		} else {
+			// app config/vhost/{template}.ctp
+			return dirname(dirname(dirname(__FILE__))) . DS . 'config' . DS . 'vhost' . DS;
+		}
+	}
+}
